30c330f116df0a37357ca5d5bf4f26ea8def846e
Aisameshima <aisame0311@gmail.com>
2025-07-24 11:18:06 +0900

    commit

--- test/src/javascript/fade-in.js
+++ test/src/javascript/fade-in.js
@@ -1,13 +1,17 @@
 $(function () {
-  $(window).scroll(function(){
-    var windowHeight = $(window).height(),
-    scrollY = $(window).scrollTop(),
-      buttonPosition = $('.work-item').offset().top;
-    if(scrollY > buttonPosition - windowHeight){
-      $('.work-item').addClass('start_animation_scroll');
-    }else{
-      $('.work-item').removeClass('start_animation_scroll');
-    }
-  });
-});
+  $(window).on('scroll', function () {
+    var windowHeight = $(window).height();
+    var scrollY = $(window).scrollTop();
 
+    $('.work-item').each(function () {
+      var $this = $(this);
+      var itemOffset = $this.offset().top;
+
+      if (scrollY > itemOffset - windowHeight + 100) {
+        $this.addClass('start_animation_scroll');
+      }
+    });
+  });
+
+  $(window).trigger('scroll'); // 初期表示時にも発火
+});
